bcb34a1483ee01a204e227800cd8ea0f920926cc
hyeonjiseon <hyeonjiiseon@gmail.com>
2020-07-06 19:24:53 +0900

    lab01-1

---
+++ lab01-1.py
@@ -0,0 +1,10 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #이름과 나이를 저장했다가 인사와 함께 출력하는 프로그램
+>>> name = "홍길동"
+>>> age = 21
+>>> print("안녕하세요?", "저는", age, "살", name, "입니다.")
+안녕하세요? 저는 21 살 홍길동 입니다.
+>>> print(age)
+21
+>>> 
