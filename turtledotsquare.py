c70755c8ea9aab0963713efbe265e580175146ba
hyeonjiseon <hyeonjiiseon@gmail.com>
2020-07-14 11:16:02 +0900

    turtledotsquare

---
+++ turtledotsquare.py
@@ -0,0 +1,17 @@
+#점선으로 사각형 그리기
+import turtle
+turtle.setup(width=500, height=500)
+t=turtle.Pen()
+t.pencolor("dark green")
+t.width(3)
+edges=4
+length=500
+dot_size=length/20
+
+for i in range(edges):
+    for j in range(edges):
+        t.forward(dot_size)
+        t.up() #펜을 들어서 이동
+        t.forward(dot_size)
+        t.down() #펜을 내려서 그리기 시작
+    t.right(90)
